398de8cceef5db07cf9532449b6a8ec046790c17
Hermitter <chacin.carlos9798@gmail.com>
2021-01-19 14:25:37 -0500

    Add empty tv query functions

---
+++ src/api/query/tv.rs
@@ -0,0 +1,3 @@
+pub async fn tv_channels(&self) -> Result<(), Error> {todo!()}
+pub async fn tv_active_channel(&self) -> Result<(), Error> {todo!()}
+pub async fn tv_input(&self) -> Result<(), Error> {todo!()}
