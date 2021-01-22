ddf3660927067f407f28c850fa137bf04069f275
Hermitter <chacin.carlos9798@gmail.com>
2021-01-22 18:24:16 -0500

    Add crate comments in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
+//! # Roku ECP
+//!
+//! This crate provides a wrapper over the [Roku External Control
+//! Protocol](https://developer.roku.com/docs/developer-program/debugging/external-control-api.md)
+//! which enables a Roku device to be controlled over a local area network
+//! through a RESTful API.
+//!
+//! # Example Usage
+//! The crate is fairly straightforward since all relevant methods revolve
+//! around the [Device](crate::Device) struct.
+//!
+//! For a full code representation, view the [examples folder](https://github.com/Hermitter/roku-ecp-rs/tree/main/examples).
+//! ```
+//! use roku_ecp::{Device, Key, SearchRequest, SearchType};
+//! let roku = Device::new("192.168.1.138").unwrap(); // remember to change the IP.
+//!
+//! // Print information on the Roku device
+//! println!("{:?}", roku.device_info().await.unwrap());
+//!
+//! // Press the play button
+//! roku.key_press(Key::Play).await.unwrap();
+//!
+//! // Search for something
+//! let search = SearchRequest::new("The Mandalorian")
+//!     .search_type(SearchType::TvShow)
+//!     .season(2)
+//!
+//! roku.search(search).await.unwrap();
+//! ```
+//!
+//! # Features
+//! Currently Implemented:
+//! - Queries for device, media player, and app information.
+//! - Key press/down/up events for various [keys](crate::Key).
+//! - Launching/Installing Applications.
+//! - Searching through Roku's Search UI.
+//!
+//! Possible Features for the Future:
+//! - Scanning the local network for Roku devices.
+//! - Inputs for  accelerometer, orientation, gyroscope, magnetometer, touch,
+//!   and multi-touch.
+//! - Roku TV commands for queries and inputs.
+//!
+//! # Dependencies
+//!
+//! **Operating Systems**
+//!
+//! Windows and macOS: None
+//!
+//! Linux: OpenSSL + headers > v1.0.1
+//! - Debian: `sudo apt install libssl-dev`
+//! - Fedora: `sudo dnf install openssl-devel`
+//!
+//! **Async Runtime**
+//!
+//! This crate requires an asynchronous runtime such as
+//! [tokio](https://github.com/tokio-rs/tokio) or
+//! [async-std](https://github.com/async-rs/async-std).
+//!
+//!
+
 pub use error::Error;
 use surf::Client;
 mod api;
@@ -9,11 +70,11 @@ use url::Url;
 /// HTTP port for communicating with the ECP RESTful service.
 const ECP_PORT: &str = "8060";
 
-/// A Roku device to communicate with via the [External Control Protocol](https://developer.roku.com/docs/developer-program/debugging/external-control-api.md).
+/// A Roku device.
 #[derive(Debug)]
 pub struct Device {
     /// Base URL of the Roku device's IP and port.
-    pub url: url::Url,
+    url: url::Url,
     /// Http client for communicating with Roku device.
     http: Client,
 }
